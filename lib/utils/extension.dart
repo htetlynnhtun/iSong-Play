a01ba7f7546c3e8c138a877bbc8b98c68454d8cf
Htet Lynn Htun <25409428+HtetLynnHtun@users.noreply.github.com>
2022-05-30 10:19:12 +0630

    Add extension method - showToast

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:music_app/resources/colors.dart';
 
-extension NavigationUtil on Widget{
-  void navigateToNextPageWithNavBar(BuildContext context,Widget page){
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context)=>page)
+extension NavigationUtil on Widget {
+  void navigateToNextPageWithNavBar(BuildContext context, Widget page) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => page));
+  }
+}
+
+extension ToastMessage on Widget {
+  // Todo: Customize toast message ui.
+  Future<bool?> showToast(String message) {
+    return Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: primaryColor,
     );
   }
 }
