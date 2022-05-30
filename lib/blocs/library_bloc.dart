31d35334680dcb1c02f4ccc986a1436e0885b1b3
Htet Lynn Htun <25409428+HtetLynnHtun@users.noreply.github.com>
2022-05-30 10:20:27 +0630

    Implement onTapAddToLibrary

---
+++ lib/blocs/library_bloc.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:music_app/persistance/song_dao.dart';
+import 'package:music_app/services/downloader_service.dart';
+import 'package:music_app/vos/song_vo.dart';
+
+class LibraryBloc extends ChangeNotifier {
+  final _songDao = SongDao();
+  final _downloader = DownloaderService();
+
+  Future<AddToLibraryResult> onTapAddToLibrary(SongVO songVO) {
+    final completer = Completer<AddToLibraryResult>();
+    final songInHive = _songDao.getItem(songVO.id);
+
+    if (songInHive == null) {
+      _downloader.requestDownload(
+        songVO,
+        onProgress: (received, total) {
+          if (total != -1) {
+            songVO.percent = received / total;
+            // notifyListeners();
+          }
+        },
+        onDownloadFinished: (filePath) async {
+          songVO.filePath = filePath;
+          songVO.isDownloadFinished = true;
+          await _songDao.saveItem(songVO);
+          print("wtbug: Download finished: filePath: ${songVO.filePath}");
+          // notifyListeners();
+          completer.complete(AddToLibraryResult.success);
+        },
+      );
+    } else {
+      completer.complete(AddToLibraryResult.alreadyInLibrary);
+    }
+
+    return completer.future;
+  }
+}
+
+enum AddToLibraryResult {
+  success,
+  alreadyInLibrary,
+}
